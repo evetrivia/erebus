f02f468681e62ed39f24bb6b59a76728e4537a37
Regner Blok-Andersen <shadowdf@gmail.com>
2015-05-02 17:41:10 +0000

    finished with version 1rc I think

--- erebus/modules/__init__.py
+++ erebus/modules/__init__.py
@@ -4,5 +4,8 @@ from erebus.extensions import api
 
 
 def register_apis():
+    from erebus.modules.root import register_apis
+    register_apis(api)
+    
     from erebus.modules.questions import register_apis
     register_apis(api)
