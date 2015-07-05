5815f7ec29fb52c1417a783291452b5d8302e9c0
Regner Blok-Andersen <shadowdf@gmail.com>
2015-07-05 02:27:31 +0000

    Minor updates. Requiring the new version of Thanatos.

--- erebus/settings.py
+++ erebus/settings.py
@@ -8,30 +8,17 @@ class Config(object):
     
     DEBUG = False
 
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', '127.0.0.1')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'vagrant')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'vagrant')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'thanatos')
+
 
 class ProdConfig(Config):
     """ Production configuration. """
-    
-    MYSQL_HOST = os.environ.get('MYSQL_HOST')
-    MYSQL_USER	= os.environ.get('MYSQL_USER')
-    MYSQL_PASSWORD	= os.environ.get('MYSQL_PASSWORD')
-    MYSQL_DB = os.environ.get('MYSQL_DB')
 
 
 class DevConfig(Config):
     """ Development configuration. """
     
     DEBUG = True
-
-    MYSQL_HOST = '127.0.0.1'
-    MYSQL_USER	= 'vagrant'
-    MYSQL_PASSWORD	= 'vagrant'
-    MYSQL_DB = 'thanatos'
-
-class C9Config(DevConfig):
-    """ Development configuration for the C9 IDE """
-    
-    MYSQL_HOST = os.environ.get('IP')
-    MYSQL_USER	= os.environ.get('C9_USER')
-    MYSQL_PASSWORD	= ''
-    MYSQL_DB = 'c9'
--- manage.py
+++ manage.py
@@ -1,26 +1,15 @@
 
 
-import os
-
 from flask.ext.script  import Manager, Shell, Server, Command
 
 from erebus.app import create_app
-from erebus.settings import C9Config, ProdConfig, DevConfig
+from erebus.settings import DevConfig
 from erebus.extensions import mysql
-from thanatos.database.db_utils import download_tables, load_tables_from_files, update_sql_stored_procs
+from thanatos.database.db_utils import load_tables_from_files, update_sql_stored_procs
 
 
-if os.environ.get('PRODUCTION') is not None:
-    app = create_app(ProdConfig)
-    server = Server()
-
-elif os.environ.get('C9_PROJECT') is not None:
-    app = create_app(C9Config)
-    server = Server(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
-
-else:
-    app = create_app(DevConfig)
-    server = Server()
+app = create_app(DevConfig)
+server = Server()
     
 manager = Manager(app)
 
@@ -30,17 +19,22 @@ def _make_context(Command):
 
     return {'app': app, 'db': mysql.connection}
 
-class UpdateDatabase(Command):
-    """ Runs the Thanatos database methods to update the databae tables. """
+class UpdateTables(Command):
+    """ Runs the Thanatos database methods to update the database tables. """
     
     def run(self):
-        download_tables()
         load_tables_from_files(mysql.connection)
+
+class UpdateProcs(Command):
+    """ Runs the Thanatos database methods to update the database procedures. """
+
+    def run(self):
         update_sql_stored_procs(mysql.connection)
 
 manager.add_command('server', server)
 manager.add_command('shell', Shell(make_context=_make_context))
-manager.add_command('update_db', UpdateDatabase())
+manager.add_command('update_tables', UpdateTables())
+manager.add_command('update_procs', UpdateProcs())
 
 if __name__ == '__main__':
     manager.run()
